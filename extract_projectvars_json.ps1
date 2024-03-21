0cc39919872373bf410728781f04dc154dd7605b
Purushotham B <62295989+puru7791@users.noreply.github.com>
2024-03-21 12:11:47 +0530

    Update extract_projectvars_json.ps1

--- extract_projectvars_json.ps1
+++ extract_projectvars_json.ps1
@@ -1,11 +1,11 @@
 PowerShell script to extract project-specific variables from Octopus Deploy in JSON format:
  
 # Replace these variables with your Octopus Deploy API key and server URL
-$apiKey = "YOUR_API_KEY"
-$octopusServerUrl = "YOUR_OCTOPUS_SERVER_URL"
+$apiKey = "$env:OCTOPUS_API_KEY"
+$octopusServerUrl = "$env:OCTOPUS_SERVER_URL"
  
 # Replace this with the name of the project whose variables you want to extract
-$projectName = "YOUR_PROJECT_NAME"
+$projectName = "$env:ProjectName"
  
 # Base64 encode the API key
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($apiKey)"))
